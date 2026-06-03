f70bd82e84305a90ddfa7bff896c06202a4570ee
Adhil Ahmmed <adhilahammed7792@gmail.com>
2026-06-03 16:53:30 +0530

    update backend

--- Front-end/Nextify/src/Services/serverURL.js
+++ Front-end/Nextify/src/Services/serverURL.js
@@ -1,6 +1,6 @@
-const serverURL = process.env.REACT_APP_BACKEND_URL || (
+const serverURL = import.meta.env.VITE_BACKEND_URL || (
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-    ? "http://localhost:5000"
+    ? "https://nextify-1.onrender.com"
     : window.location.origin
 );
 
