217e9b8b972f965f1e69ce416ae4e6acb4694ae8
Adhil Ahmmed <adhilahammed7792@gmail.com>
2026-06-03 15:29:12 +0530

    'update'

--- Front-end/Nextify/src/Services/serverURL.js
+++ Front-end/Nextify/src/Services/serverURL.js
@@ -1,7 +1,7 @@
 const serverURL = process.env.REACT_APP_BACKEND_URL || (
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "https://nextify-egkj.onrender.com"
+    : window.location.origin
 );
 
 export default serverURL;
